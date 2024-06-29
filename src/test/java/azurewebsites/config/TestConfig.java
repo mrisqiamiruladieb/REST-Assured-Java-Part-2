551ad16bb8fd80e6956bc228a0ac8764a522bd93
adieb <mrizqiamiruladieb@gmail.com>
2024-06-29 17:41:42 +0700

    feat: configure the base url

---
+++ src/test/java/azurewebsites/config/TestConfig.java
@@ -0,0 +1,10 @@
+package azurewebsites.config;
+
+public class TestConfig {
+
+    private String baseUrl = "https://fakerestapi.azurewebsites.net/api/v1";
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
