fb023d7b13ef16021ea239b8a90865b8990a4c6c
adieb <mrizqiamiruladieb@gmail.com>
2024-06-30 18:01:48 +0700

    feat: convert to lower case with space (helper for commit messages)

---
+++ src/main/java/convertToLowerCaseWithSpace.java
@@ -0,0 +1,29 @@
+public class convertToLowerCaseWithSpace {
+
+    public static void main(String[] args) {
+        String inputString = "convertToLowerCaseWithSpace";
+        String outputString = convertToLowerCaseWithSpace(inputString);
+        System.out.println(outputString); // Output: "success get user by id"
+    }
+
+    public static String convertToLowerCaseWithSpace(String inputString) {
+        StringBuilder result = new StringBuilder();
+
+        // Append the first character as it is
+        result.append(inputString.charAt(0));
+
+        // Iterate through the rest of the string
+        for (int i = 1; i < inputString.length(); i++) {
+            char currentChar = inputString.charAt(i);
+
+            // If current character is uppercase, add space and lowercase version
+            if (Character.isUpperCase(currentChar)) {
+                result.append(' ').append(Character.toLowerCase(currentChar));
+            } else {
+                result.append(currentChar);
+            }
+        }
+
+        return result.toString();
+    }
+}
