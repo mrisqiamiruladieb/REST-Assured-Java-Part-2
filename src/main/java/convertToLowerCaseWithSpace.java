455a0420fda64e27af693ae5d1ecdcdb0b51c1e4
adieb <mrizqiamiruladieb@gmail.com>
2024-06-30 21:00:55 +0700

    feat: validate get data with unregister id

--- src/main/java/convertToLowerCaseWithSpace.java
+++ src/main/java/convertToLowerCaseWithSpace.java
@@ -1,7 +1,7 @@
 public class convertToLowerCaseWithSpace {
 
     public static void main(String[] args) {
-        String inputString = "convertToLowerCaseWithSpace";
+        String inputString = "validateGetDataWithUnregisterId";
         String outputString = convertToLowerCaseWithSpace(inputString);
         System.out.println(outputString); // Output: "success get user by id"
     }
--- src/test/java/azurewebsites/scenarios/getUserById.java
+++ src/test/java/azurewebsites/scenarios/getUserById.java
@@ -73,4 +73,51 @@ public class getUserById {
                 .body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaFilePath))) // Validate the json schema response
                 .time(lessThan(5000L)); // Validate the response time – in milliseconds
     }
+
+    @Test
+    public void validateGetDataWithUnregisterId() {
+
+        // Save path file not found get user by id response schema
+        String jsonSchemaFilePath = "src/test/java/azurewebsites/schema/Users/notFoundGetUserByIdResponseSchema.json";
+
+        System.out.println("--------------------Request----------------------");
+
+        Response response = given().log().all()
+                .accept("*/*")
+                .and()
+                .pathParams("id", "11")
+                .when()
+                .get(apiEndpoints.getUserById()); // get endpoints to get user by id
+
+        System.out.println("--------------------Response----------------------");
+
+        // Get response code
+        int statusCode = response.getStatusCode();
+        System.out.println("Response Status Codes: " + statusCode);
+
+        // Get response body
+        String responseBody = response.thenReturn().asPrettyString();
+        System.out.println("Response Body:\n" + responseBody);
+
+        //Assert
+        Assert.assertEquals(statusCode, 404, "Check the status code is 404");
+        Assert.assertEquals(response.getContentType(), "application/problem+json; charset=utf-8; v=1.0", "Check the content type is application/problem+json; charset=utf-8; v=1.0");
+        Assert.assertFalse(responseBody.isEmpty(), "Check the response body is not empty");
+        Assert.assertEquals(responseBody.contains("type"), true, "Check the presence of property type");
+        Assert.assertTrue(responseBody.contains("title"), "Check the presence of property title");
+        Assert.assertTrue(responseBody.contains("status"), "Check the presence of property status");
+        Assert.assertTrue(responseBody.contains("traceId"), "Check the presence of property traceId");
+
+        response.then()
+                .assertThat()
+                .body("type", not(empty())) // Assert a non-empty variable
+                .body("type", equalTo("https://tools.ietf.org/html/rfc7231#section-6.5.4"))
+                .body("title", not(empty())) // Assert a non-empty variable
+                .body("title", equalTo("Not Found"))
+                .body("status", is(greaterThan(0))) // Assert a non-empty variable
+                .body("status", equalTo(404))
+                .body("traceId", not(empty())) // Assert a non-empty variable
+                .body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaFilePath))) // Validate the json schema response
+                .time(lessThan(5000L)); // Validate the response time – in milliseconds
+    }
 }
