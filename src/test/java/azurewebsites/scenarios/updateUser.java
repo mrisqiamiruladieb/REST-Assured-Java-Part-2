559c2641b31d254cc7b27e3e2176960ce5ebaacc
adieb <mrizqiamiruladieb@gmail.com>
2024-07-01 14:37:53 +0700

    feat: validate update user with blank request body field

--- src/test/java/azurewebsites/scenarios/updateUser.java
+++ src/test/java/azurewebsites/scenarios/updateUser.java
@@ -364,4 +364,38 @@ public class updateUser {
                 .assertThat()
                 .time(lessThan(5000L)); // Validate the response time – in milliseconds
     }
+
+    @Test
+    public void validateUpdateUserWithBlankRequestBodyField() {
+
+        System.out.println("--------------------Request----------------------");
+
+        Response response = given().log().all()
+                .accept("*/*")
+                .contentType(ContentType.JSON)
+                .body("{}")
+                .and()
+                .pathParams("id", "4")
+                .when()
+                .put(apiEndpoints.getUserById()); // get endpoints to update user by id
+
+        System.out.println("--------------------Response----------------------");
+
+        // Get response code
+        int statusCode = response.getStatusCode();
+        System.out.println("Response Status Codes: " + statusCode);
+
+        // Get response body
+        String responseBody = response.thenReturn().asPrettyString();
+        System.out.println("Response Body:\n" + responseBody);
+
+        //Assert
+        Assert.assertEquals(statusCode, 400, "Check the status code is 400");
+        Assert.assertEquals(response.getContentType(), "application/json; charset=utf-8; v=1.0", "Check the content type is application/json; charset=utf-8; v=1.0");
+        Assert.assertFalse(responseBody.isEmpty(), "Check the response body is not empty");
+
+        response.then()
+                .assertThat()
+                 .time(lessThan(5000L)); // Validate the response time – in milliseconds
+    }
 }
