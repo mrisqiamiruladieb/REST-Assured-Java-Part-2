dd02e35b4d2f09796da048c7b8d392210f4cfae1
adieb <mrizqiamiruladieb@gmail.com>
2024-07-01 14:18:14 +0700

    feat: validate update user with blank id path params

--- src/test/java/azurewebsites/scenarios/updateUser.java
+++ src/test/java/azurewebsites/scenarios/updateUser.java
@@ -210,4 +210,63 @@ public class updateUser {
                 .body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaFilePath))) // Validate the json schema response
                 .time(lessThan(5000L)); // Validate the response time – in milliseconds
     }
+
+    @Test
+    public void validateUpdateUserWithBlankIdPathParams() {
+
+        // get create user success request body
+        JSONObject payload = dataUsers.successUpdateUser();
+
+        // Save path file valid create user response schema
+        String jsonSchemaFilePath = "src/test/java/azurewebsites/schema/Users/badRequestGetUserByIdResponseSchema.json";
+
+        System.out.println("--------------------Request----------------------");
+
+        Response response = given().log().all()
+                .accept("*/*")
+                .contentType(ContentType.JSON)
+                .body(payload.toString())
+                .and()
+                .pathParams("id", " ")
+                .when()
+                .put(apiEndpoints.getUserById()); // get endpoints to update user by id
+
+        System.out.println("--------------------Response----------------------");
+
+        // Get response code
+        int statusCode = response.getStatusCode();
+        System.out.println("Response Status Codes: " + statusCode);
+
+        // Get response body
+        String responseBody = response.thenReturn().asPrettyString();
+        System.out.println("Response Body:\n" + responseBody);
+
+        //Assert
+        Assert.assertEquals(statusCode, 400, "Check the status code is 400");
+        Assert.assertEquals(response.getContentType(), "application/problem+json; charset=utf-8", "Check the content type is application/problem+json; charset=utf-8");
+        Assert.assertFalse(responseBody.isEmpty(), "Check the response body is not empty");
+        Assert.assertEquals(responseBody.contains("type"), true, "Check the presence of property type");
+        Assert.assertTrue(responseBody.contains("title"), "Check the presence of property title");
+        Assert.assertTrue(responseBody.contains("status"), "Check the presence of property status");
+        Assert.assertTrue(responseBody.contains("traceId"), "Check the presence of property traceId");
+        Assert.assertTrue(responseBody.contains("errors"), "Check the presence of property errors");
+        Assert.assertTrue(responseBody.contains("id"), "Check the presence of property id");
+
+        response.then()
+                .assertThat()
+                .body("type", not(empty())) // Assert a non-empty variable
+                .body("type", equalTo("https://tools.ietf.org/html/rfc7231#section-6.5.1"))
+                .body("title", not(empty())) // Assert a non-empty variable
+                .body("title", equalTo("One or more validation errors occurred."))
+                .body("status", is(greaterThan(0))) // Assert a non-empty variable
+                .body("status", equalTo(400))
+                .body("traceId", not(empty())) // Assert a non-empty variable
+                .body("errors", not(empty())) // Assert a non-empty variable
+                .body("errors", hasKey("id")) // Asserts a variable has a specific key
+                .body("errors.id", not(emptyArray())) // Assert a non-empty array variable
+                .body("errors.id[0]", not(empty())) // Assert a non-empty variable
+                .body("errors.id[0]", equalTo("The value ' ' is invalid."))
+                .body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaFilePath))) // Validate the json schema response
+                .time(lessThan(5000L)); // Validate the response time – in milliseconds
+    }
 }
