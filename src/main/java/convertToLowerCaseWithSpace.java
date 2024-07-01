d9219fec7b91ddd4a3d11f41b1e03d21ac49dfd7
adieb <mrizqiamiruladieb@gmail.com>
2024-07-01 14:09:26 +0700

    feat: validate update user with non integer id path params

--- src/main/java/convertToLowerCaseWithSpace.java
+++ src/main/java/convertToLowerCaseWithSpace.java
@@ -1,7 +1,7 @@
 public class convertToLowerCaseWithSpace {
 
     public static void main(String[] args) {
-        String inputString = "validateUpdateUserWithUnregisterIdPathParams";
+        String inputString = "validateUpdateUserWithNonIntegerIdPathParams";
         String outputString = convertToLowerCaseWithSpace(inputString);
         System.out.println(outputString); // Output: "success get user by id"
     }
